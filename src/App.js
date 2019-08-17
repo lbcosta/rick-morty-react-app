0f4b22973ab6cb805b9a55e697e4c987e1ebfb68
Leonardo Costa <leocosta@edu.unifor.br>
2019-08-17 20:01:39 -0300

    :boom: Total rebuild! Let's start over :D

--- src/App.js
+++ src/App.js
@@ -1,54 +1,7 @@
-import React, { Component } from 'react';
-import './css/pure-min.css';
-import './css/side-menu.css';
-import './css/custom.css';
-import { Link } from 'react-router/lib';
+import React from "react";
 
-class App extends Component {
-
-  constructor() {
-    super()
-    this.invisible = this.invisible.bind(this);
-  }
-
-  invisible(event) {
-    event.target.style.visibility = "hidden";
-
-  }
-
-  render() {
-    let btn, img;
-    if (this.props.location.pathname === "/") {
-      btn = (
-        <Link className="waves-effect waves-light btn-large"
-          to="/characters/1"
-          onClick={this.invisible}>SHOW ME WHAT YOU GOT</Link>
-
-      )
-      
-      img = (<img src="../showme.jpeg" id="show-me" alt=""/>)
-    }
-
-    return (
-      <div >
-        <div id="main">
-          <div className="header my-header">
-            <h1>RICK AND MORTY CARD WIKI</h1>
-            <h2>a simple page for an awesome show</h2>
-            <nav className="my-nav-wrapper">
-            {btn}
-              <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="https://github.com/lbcosta/rick-morty-api-react-app" target="_blank" rel="noopener noreferrer">Github</a></li>
-              </ul>
-            </nav>
-            
-          </div>
-          {img}
-          {this.props.children}
-        </div>
-      </div>
-    );
-  }
+function App() {
+  return <h1>Pickle Rick!!!!</h1>;
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
-import { Router, Route, browserHistory } from 'react-router';
-import ContentBox from './components/content-view';
 
-ReactDOM.render(
-    (<Router history={browserHistory}>
-        <Route path="/" component={App}>
-            <Route path="/characters/:page" component={ContentBox} />
-        </Route>
-    </Router>),
-    document.getElementById('root')
-);
-//      
-registerServiceWorker();
+ReactDOM.render(<App />, document.getElementById('root'));
